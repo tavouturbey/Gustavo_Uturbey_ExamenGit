0310d26c5c5687bf270164ee0bea014d73c69dd9
GustavoUturbey-Alumn <GustavoUturbey-Alumn@CEACFP10360>
2023-05-26 13:04:06 +0200

    Se suben todos los archivos

---
+++ ExamenEntornosEstaVezSi/src/pkg/Clase.java
@@ -0,0 +1,28 @@
+package pkg;
+import java.util.Scanner;
+public class Clase {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Pedir edad al usuario
+        System.out.print("Introduce tu edad: ");
+        int edad = scanner.nextInt();
+
+        // Comprobar si la edad está entre 18 y 65
+        if (edad < 18 || edad > 65) {
+            System.out.println("La edad debe estar entre 18 y 65 años.");
+        } else {
+            // Pedir DNI al usuario
+            System.out.print("Introduce tu DNI (8 números y 1 letra): ");
+            String dni = scanner.next();
+
+            // Comprobar si el DNI cumple con el formato de 8 números y 1 letra
+            if (!dni.matches("\\d{8}[A-Za-z]")) {
+                System.out.println("El DNI debe tener 8 números y 1 letra.");
+            } else {
+                System.out.println("Edad y DNI válidos.");
+            }
+        }
+    }
+}
